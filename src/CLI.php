325c74055186d055537b53ee1a15d7ec5a0bac9a
Aubrey Portwood <code@aubreypwd.com>
2023-01-14 14:26:51 -0700

    Implement required methods.

--- src/CLI.php
+++ src/CLI.php
@@ -3,4 +3,6 @@
 namespace aubreypwd\PHP_CLI;
 
 abstract class CLI extends \splitbrain\phpcli\CLI {
+	abstract protected function setup( \splitbrain\phpcli\Options $options );
+	abstract protected function main( \splitbrain\phpcli\Options $options );
 }
