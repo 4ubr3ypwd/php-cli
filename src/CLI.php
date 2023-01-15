71d5bc5984afa59b6f68ea3c0248b9edebfa234a
Aubrey Portwood <code@aubreypwd.com>
2023-01-15 01:09:18 -0700

    Fix issue where bad options get an exception.
    Just use $this->invalidate_unexplained_options( $options ); in your
    class.

--- src/CLI.php
+++ src/CLI.php
@@ -3,13 +3,29 @@
 namespace aubreypwd\PHP_CLI;
 
 use \splitbrain\phpcli\Options;
+use \splitbrain\phpcli\Colors;
 
 abstract class CLI extends \splitbrain\phpcli\CLI {
+
+	public $colors;
+
+	public $_options = [];
+
+	public function __construct() {
+
+		$this->colors = new Colors();
+
+		parent::__construct();
+	}
 
 	abstract protected function setup( Options $options );
 	abstract protected function main( Options $options );
 
 	protected function has_command( string $command ) : bool {
+
+		if ( empty( $command ) ) {
+			return false;
+		}
 
 		$exec = exec( "command -v {$command}" );
 
@@ -17,33 +33,28 @@ abstract class CLI extends \splitbrain\phpcli\CLI {
 			return false;
 		}
 
-		return empty( $exec ) ? false : basename( trim( $exec ) ) === $command;
+		if ( empty( $exec ) ) {
+			return false;
+		}
+
+		return basename( trim( $exec ) ) === $command;
 	}
 
 	protected function get_php_version() : string {
-
-		$command = $this->safe_exec( "php -r 'echo phpversion() . \"\n\";' | sed 's/ *$//g'" );
-
-		return is_string( $command ) ? trim( $command ) : '';
+		return $this->get_last_line( $this->safe_exec( "php -r 'echo phpversion() . \"\n\";' | sed 's/ *$//g'" ) );
 	}
 
 	protected function get_working_dir() : string {
-
-		$command = $this->safe_exec( 'git status' );
-
-		return is_string( $command ) ? trim( $command ) : '';
+		return $this->get_last_line( $this->safe_exec( 'pwd' ) );
 	}
 
 	protected function get_working_dirname() : string {
-
-		$command = $this->safe_exec( 'echo "${PWD##*/}"' );
-
-		return is_string( $command ) ? trim( $command ) : '';
+		return $this->get_last_line( $this->safe_exec( 'echo "${PWD##*/}"' ) );
 	}
 
 	private function safe_exec( string $command ) : array {
 
-		if ( ! $this->has_command( $command ) ) {
+		if ( ! $this->has_command( strtok( $command, ' ' ) ) ) {
 
 			return [
 				'last_line' => '',
@@ -59,35 +70,37 @@ abstract class CLI extends \splitbrain\phpcli\CLI {
 		];
 	}
 
-	private function get_safe_exec_output( array $result, string $as = 'array' ) : string|array {
+	private function get_output( array $result, string $as = 'array' ) : string|array {
 
 		if ( 'array' !== $string && 'string' !== $as ) {
 			throw new \InvalidArgumentException( '$as must be set to array|string.' );
 		}
 
 		if ( ! isset( $array['output'] ) || ! is_array( $array['output'] ) ) {
-			throw new \InvalidArgumentException( 'We can only work with an array from safe_exec().' );
+			throw new \InvalidArgumentException( 'We can only work with an array from exec().' );
 		}
 
 		return 'array' === $as ? $array['output'] : implode( "\n", $output );
 	}
 
-	private function get_safe_execute_last_line( array $result ) : string {
-		return isset( $result['last_line'] ) ? trim( $result['last_line'] ) : '';
+	private function get_last_line( array $exec ) : string {
+		return isset( $exec['last_line'] ) ? trim( $exec['last_line'] ) : '';
 	}
 
 	protected function get_arg( Options $options, int $position ) : string {
 
 		$args = $options->getArgs();
 
-		return isset( $args[ $position ] ) ? trim( $args[ $position ] ) : '';
+		return isset( $args[ $position ] )
+			? $args[ $position ]
+			: '';
 	}
 
 	protected function get_args( Options $options ) : array {
 		return $options->getArgs();
 	}
 
-	protected function register_argument( Options $options, string $arg, string $help, bool $required = true, string $command = '' ) : void {
+	protected function explain_argument( Options $options, string $arg, string $help, bool $required = true, string $command = '' ) : void {
 		$options->registerArgument( $arg, $help, $required, $command );
 	}
 
@@ -95,7 +108,54 @@ abstract class CLI extends \splitbrain\phpcli\CLI {
 		$options->setHelp( $help );
 	}
 
-	protected function register_option( $options, string $long, string $help, $short = null, bool $needsarg = false, string $command = '' ) : void {
+	protected function explain_option( Options $options, string $long, string $help, mixed $short = null, bool $needsarg = false, string $command = '' ) : void {
 		$options->registerOption( $long, $help, $short, $needsarg, $command );
+
+		$this->_options[ $long ] = $short;
+	}
+
+	protected function get_opt( Options $options, $option ) : bool|string {
+		return $options->getOpt( $option );
+	}
+
+	protected function show_help( Options $options ) {
+		echo $options->help();
+	}
+
+	protected function invalidate_unexplained_options( Options $options ) {
+
+		foreach ( $_SERVER['argv'] as $position => $arg ) {
+
+			if ( 0 === intval( $position ) ) {
+				continue;
+			}
+
+			if ( '-' !== substr( $arg, 0, 1 ) ) {
+				continue;
+			}
+
+			if ( ! strstr( $arg, '-' ) ) {
+				continue;
+			}
+
+			$base = str_replace( array( '--', '-' ), '', $arg );
+
+			if ( in_array( $base, array_values( $this->_options ), true ) ) {
+				continue;
+			}
+
+			if ( in_array( $base, array_keys( $this->_options ), true ) ) {
+				continue;
+			}
+
+			$this->explain_option(
+				$options,
+				$base,
+				$this->colors->wrap( 'Invalid option.', $this->colors::C_RED ),
+				strstr( $arg, '--' )
+					? ''
+					: substr( $base, 0, 1 )
+			);
+		}
 	}
 }
